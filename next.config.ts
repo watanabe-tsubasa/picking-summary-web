13eb9b015ba4893f86f506bf603a0f2d895156cd
Watanabe Tsubasa (渡邊 翼) <watanabe-tsuba@aeonpeople.biz>
2025-05-21 10:59:50 +0900

    del nodejs-polars from nextconfig

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   webpack: (config, { isServer }) => { if (isServer) { config.externals.push({ 'nodejs-polars': 'commonjs nodejs-polars' }); } return config },
-  serverExternalPackages: ['@napi-rs/canvas', 'nodejs-polars'],
+  serverExternalPackages: ['@napi-rs/canvas'],
 };
 
 export default nextConfig;
